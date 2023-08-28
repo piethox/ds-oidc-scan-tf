0c78755b547c613e5a08b1f60a1a843942cf343e
piethox <dsuntama@gmail.com>
2023-08-28 22:46:23 +0800

    edit backend file

--- ecr/backend.tf
+++ ecr/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce2-tfstate-bkt"
-    key    = "ds2-test-chat.tfstate"
+    key    = "ds2-test-ecr.tfstate"
     region = "ap-southeast-1"
   }
 }
--- ecs/backend.tf
+++ ecs/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "sctp-ce2-tfstate-bkt"
-    key     = "ds2-test-chat.tfstate"
+    key     = "ds2-test-ecs.tfstate"
     region  = "ap-southeast-1"
   }
 }
